8c5e41b8bbd3a5a87edd99bac075349f633ad8be
Macgyver Martins <macgyver.martins@gmail.com>
2017-06-19 22:43:06 -0400

    create migration to add description and timestamp to the aricles tabls

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170620022252) do
+ActiveRecord::Schema.define(version: 20170620022918) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
+    t.text "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
