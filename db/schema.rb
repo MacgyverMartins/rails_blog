d6ca77f0b79806ac084899b1789a2b77baa1edb8
Macgyver Martins <macgyver.martins@gmail.com>
2017-06-24 20:00:18 -0400

    Add secure password and attribute to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170624230704) do
+ActiveRecord::Schema.define(version: 20170624235616) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -20,11 +20,7 @@ ActiveRecord::Schema.define(version: 20170624230704) do
     t.integer "user_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+# Could not dump table "users" because of following StandardError
+#   Unknown type 'sting' for column 'password_digest'
 
 end
