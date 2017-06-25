5612aadcc15e4cd1d856e4d71db75c66ad0ddead
Macgyver Martins <macgyver.martins@gmail.com>
2017-06-25 19:23:05 -0400

    Add user show page, listing user articles and integration with gravatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
   end
@@ -16,6 +20,7 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
