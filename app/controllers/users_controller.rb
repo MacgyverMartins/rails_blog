d875afd3c703c20603234c291b2a185960c8fd78
Macgyver Martins <macgyver.martins@gmail.com>
2017-07-01 19:05:58 -0400

    Restrict actions in Controllers for non-authenticated users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,13 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :show]
+  before_action :require_same_user, only: [:edit, :update]
 
   def index
-    #@users = User.all
-    @users = User.paginate(page: params[:page], per_page: 2)
+    @users = User.paginate(page: params[:page], per_page: 5)
   end
-
-  def show
-    @user_articles = @user.articles.paginate(page: params[:page], per_page: 1)
-  end
-
   def new
     @user = User.new
   end
-
   def create
     @user = User.new(user_params)
     if @user.save
@@ -23,10 +17,8 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-
   def edit
   end
-
   def update
     if @user.update(user_params)
       flash[:success] = "Your account was updated successfully"
@@ -35,13 +27,20 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
-
+  def show
+    @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
+  end
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
-
   def set_user
     @user = User.find(params[:id])
+  end
+  def require_same_user
+    if current_user != @user
+      flash[:danger] = "You can only edit your own account"
+      redirect_to root_path
+    end
   end
 end
