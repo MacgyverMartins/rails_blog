9355c3e7543ddf03ff160a74c9f01dc58e91b577
Macgyver Martins <macgyver.martins@gmail.com>
2017-07-01 20:33:39 -0400

    Add admin user funcionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,16 @@
 class User < ActiveRecord::Base
-  has_many :articles
-
+  has_many :articles, dependent: :destroy
   before_save { self.email = email.downcase }
 
   validates :username, presence: true,
-            uniqueness: {case_sensitive: false},
-            length: {minimun: 3, maximum: 25}
+    uniqueness: { case_sensitive: false },
+    length: { minimum: 3, maximum: 25 }
 
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: {maximum: 105},
-            uniqueness: {case_sensitive: false},
-            format: {with: VALID_EMAIL_REGEX}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :email, presence: true, length: { maximum: 105 },
+    uniqueness: { case_sensitive: false },
+    format: { with: VALID_EMAIL_REGEX }
 
   has_secure_password
 end
