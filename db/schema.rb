6a3699be589eaa65a6b1eb9c6e481147085dd804
Macgyver Martins <macgyver.martins@gmail.com>
2017-07-02 12:18:04 -0400

    Add Category model and unit tests for that

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170701234411) do
+ActiveRecord::Schema.define(version: 20170702154827) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -20,7 +20,19 @@ ActiveRecord::Schema.define(version: 20170701234411) do
     t.integer "user_id"
   end
 
-# Could not dump table "users" because of following StandardError
-#   Unknown type 'sting' for column 'password_digest'
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.boolean "admin", default: false
+  end
 
 end
