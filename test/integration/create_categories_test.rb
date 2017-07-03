cfccd0cf4bd57d79e23676c4e1d3c356ca73a781
Macgyver Martins <macgyver.martins@gmail.com>
2017-07-02 20:55:23 -0400

    Update Articles integration test

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -5,7 +5,8 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
     get new_category_path
     assert_template 'categories/new'
     assert_difference 'Category.count', 1 do
-      post_via_redirect categories_path, category: {name: "sports"}
+      post categories_path, params: {category: {name: "sports"}}
+      follow_redirect!
     end
     assert_template 'categories/index'
     assert_match "sports", response.body
